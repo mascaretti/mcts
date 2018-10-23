5703616dbc487a0a41868c8b95aac2551a50dc0f
Andrea Mascaretti <andrea1.mascaretti@mail.polimi.it>
2018-10-23 11:49:43 +0200

    plots

--- test/SpeedupTest/speedup_analysis.R
+++ test/SpeedupTest/speedup_analysis.R
@@ -4,25 +4,25 @@ library(tidyverse)
 
 # Nim -------------
 nim <- read_csv(file = "./test/SpeedupTest/speedup_nim.csv", col_names = FALSE,
-                col_types = list(col_factor(levels = c(1, 2, 3, 4), ordered = TRUE), col_integer(), col_double()))
+                col_types = list(col_factor(levels = c(0, 1, 2, 3, 4), ordered = TRUE), col_integer(), col_double()))
 nim <- nim %>%
   rename(size = X1, simulation = X2, elapsed = X3)
 
 ggplot(nim, mapping = aes(x = simulation, y = elapsed, colour = size)) +
   geom_smooth(se = TRUE) +
-  labs(title = "Elapsed for growing number of simulations with difference sizes - Nim")
+  labs(title = "Elapsed Time for growing number of simulations with difference sizes - Nim (0 is serial)")
 
 ggsave(filename = "./test/SpeedupTest/speedup_nim.png")
 
 # Oxo -------------------------------
 
 oxo <- read_csv(file = "./test/SpeedupTest/speedup_oxo.csv", col_names = FALSE,
-                col_types = list(col_factor(levels = c(1, 2, 3, 4), ordered = TRUE), col_integer(), col_double()))
+                col_types = list(col_factor(levels = c(0, 1, 2, 3, 4), ordered = TRUE), col_integer(), col_double()))
 oxo <- oxo %>%
   rename(size = X1, simulation = X2, elapsed = X3)
 
 ggplot(oxo, mapping = aes(x = simulation, y = elapsed, colour = size)) +
   geom_smooth(se = TRUE) +
-  labs(title = "Elapsed for growing number of simulations with difference sizes - Tic-tac-toe")
+  labs(title = "Elapsed for growing number of simulations with difference sizes - Tic-tac-toe (0 is serial)")
 
 ggsave(filename = "./test/SpeedupTest/speedup_oxo.png")
